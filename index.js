bea3d5eee894ed0528da4046c6cb01179d81a1cb
Blessings Mwandira <blessingsmwandira4@gmail.com>
2025-01-28 16:56:57 +0200

    feat: update project section with detailed project boxes and remove dynamic fetching

--- index.js
+++ index.js
@@ -24,55 +24,5 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-async function getUserProjects() {
-    const response = await fetch('https://api.github.com/users/BlessingsM02/repos');
-    const repos = await response.json();
-    
-    const projectGallery = document.getElementById('all-project');
-    repos.forEach(repo => {
-      const project = document.createElement('div');
-      project.className = 'project';
-      project.innerHTML = `
-  
-      <a
-      href=${repo.html_url}
-      target="_blank"
-      rel="noopener noreferrer"
-      class="repo">
-      <div class="repo_top">
-        <div class="repo-name">
-          <h3>${repo.name}</h3>
-        </div>
-        <p>${repo.description}</p>
-      </div>
-  
-      <div class="repo-stats">
-  
-        <div class="repo-stats-left">
-          <span>
-              ${repo.language}
-          </span>
-          <span>
-          <i class='bx bxs-star'></i>
-            ${repo.stargazers_count.toLocaleString()}
-          </span>
-          <span>
-          <i class='bx bx-git-repo-forked'></i>
-            ${repo.forks.toLocaleString()}
-          </span>
-        </div>
-  
-        <div class="repo-stats-right">
-          <span>${repo.size.toLocaleString()} KB</span>
-        </div>
-      </div>
-    </a>
-    
-      `;
-      projectGallery.appendChild(project);
-    });
-  }
-  
-  
-  getUserProjects();
+
   
